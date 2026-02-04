9bb1c65e434d75b2d76a1f8ada7c41dc522d1fbb
Darwisy <bahijdarwisyfarras@gmail.com>
2026-02-04 20:38:04 +0700

    homepage update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="h-max bg-primary dark:bg-primary-dark">
-    <header>
-      <Navbar />
-    </header>
+    <Navbar />
     <main class="pt-17">
       <RouterView />
     </main>
-    <footer class="px-4">
-      <Divider />
-      <Address />
-      <Divider />
-    </footer>
+    <Divider />
+    <Address />
+    <Divider />
+    <Footer />
   </div>
 </template>
 <script setup>
 import Divider from "./components/Divider.vue";
 import Navbar from "./components/Navbar.vue";
 import Address from "./layouts/Address.vue";
+import Footer from "./layouts/Footer.vue";
 </script>
---
+++ src/layouts/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="relative w-full bg-white dark:bg-[#0f172a]">
+    <h4 class="text-tertiary dark:text-primary text-center py-6">
+      © 2026 PT. Madinah Nurul Wisata. All Rights Reserved
+    </h4>
+  </div>
+</template>
+<script></script>
